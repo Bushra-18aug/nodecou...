136d76576d5cb89530111c890c90b9c9e7cecef5
Bushra Fateh <78873076+Bushra-18aug@users.noreply.github.com>
2022-01-03 16:12:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+function auth(){
+    var username = document.getElementById('username_input').value;
+    var password = document.getElementById('password_input').value;
+    if (username == 'admin'&& password == '1234') {
+        alert('true') 
+    } else {
+        alert('false')
+    } 
+        
+    }
+    
+    
+    
+    
